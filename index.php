c9d6a446dd6694decbb9021ac3003aae4c7ee89b
Nguyễn Ngọc Huy Tâm <108131520+anzing0983@users.noreply.github.com>
2023-03-05 22:08:43 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head><style>img[alt="www.000webhost.com"]{display:none;}</style>
+    <!-- Primary Meta Tags -->
+    <title>THÔNG TIN THANH TOÁN</title>
+    <link rel="icon" href="https://thongtinthanhtoan.com/storage/setting/thongtinthanhtoan2432_1643619909.png">
+    <meta name="title" content="THÔNG TIN THANH TOÁN">
+    <meta name="description" content="Trang web thông tin thanh toán hàng đầu Việt Nam với hơn 300 khách hàng tin tưởng sử dụng">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="thongtinthanhtoan.com">
+    <meta property="og:title" content="THÔNG TIN THANH TOÁN">
+    <meta property="og:description" content="Trang web thông tin thanh toán hàng đầu Việt Nam với hơn 300 khách hàng tin tưởng sử dụng">
+    <meta property="og:image" content="https://scontent.xx.fbcdn.net/v/t1.15752-9/334918180_1156237638422386_2751330646472111_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=i5BfSlm_fjMAX8lDUXn&_nc_oc=AQkxNT5XBrXfXSS3RIUd4gSnb3grt3ZkOLngY7uVWM8uIhtpDbae5Ptw72ACfv4C5MA76iyhwnKymPEeXyYHssig&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSngT1VvhMrXCNO7tfhaYsk0nLLoh52wNucJS8tVyMYsg&oe=642BF4EE>
+
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://thongtinthanhtoan.com/theme/profile/css/style.min.css" />
+
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="profile-header">
+            <img alt="Avatar" src="https://scontent.xx.fbcdn.net/v/t1.15752-9/334918180_1156237638422386_2751330646472111_n.png?_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=i5BfSlm_fjMAX8lDUXn&_nc_oc=AQkxNT5XBrXfXSS3RIUd4gSnb3grt3ZkOLngY7uVWM8uIhtpDbae5Ptw72ACfv4C5MA76iyhwnKymPEeXyYHssig&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSngT1VvhMrXCNO7tfhaYsk0nLLoh52wNucJS8tVyMYsg&oe=642BF4EE" class="profile-avatar">
+            <div class="profile-info">
+                <div class="profile-name">
+                    <span>Nguyễn Gia Phúc~ </span><!-- thay trong <span> (vùng để đổi tên)</span>--->
+                    <ion-icon class="tick" name="checkmark-circle" style="font-size: 18px; font-weight: 500px;">
+                    </ion-icon>
+                </div>
+                <div class="skills">
+                    <div class="skill-tag">Develooper </div>
+                    <div class="skill-tag">Designer - Coder </div>
+                </div>
+            </div>
+        </div>
+        <h4 class="title">Thông tin thanh toán </h4>
+        <div class="banks">
+                        <div class="bank">
+    <img src="https://i.imgur.com/dgZP0D7.jpg" class="bank-logo">
+    <div class="bank-info">
+        <p class="bank-name">MOMO</p>
+        <p class="bank-number">SĐT: 0777522806</p>
+        <p class="bank-owner">CTK: Nguyễn Ngọc Huy Tâm </p>
+    </div>
+    <span class="btn-copy btn-show-modal" data-number="0777522806">Copy</span>
+</div>                        <div class="bank">
+    <img src="https://i.imgur.com/fHoxejI.png" class="bank-logo">
+    <div class="bank-info">
+        <p class="bank-name">MB BANK </p>
+        <p class="bank-number">STK: 00731072007</p>
+        <p class="bank-owner">CTK:Nguyễn Ngọc Huy Tâm </p>
+    </div>
+    <span class="btn-copy " data-number="0777522806 ">Copy</span>
+</div>                        <div class="bank">
+    <img src="https://i.imgur.com/aRviP2N.png" class="bank-logo">
+    <div class="bank-info">
+        <p class="bank-name">CARDVIP.VN  </p>
+        <p class="bank-number">STK: huytam2k7@gmail.com  </p>
+        <p class="bank-owner">CTK: Nguyễn Ngọc Huy Tâm </p>
+    </div>
+    <span class="btn-copy " data-number="huytam2k7@gmail.com">Copy</span>
+</div>                        <div class="bank">
+    <img src="https://imgur.com/Dj4siYJ.png" class="bank-logo">
+    <div class="bank-info">
+        <p class="bank-name">Thẻ Siêu Rẻ</p>
+        <p class="bank-number">STK: 077522806</p>
+        <p class="bank-owner">CTK: Nguyễn Ngọc Huy Tâm</p>
+    </div>
+    <span class="btn-copy " data-number="0777522806">Copy</span>
+</div>                    </div>
+        <a href="https://www.facebook.com/NguyenNgocHuyTamxDtlacuti/" class="contact">
+            <ion-icon class="pay-here" style="font-size: 20px" name="card-outline"></ion-icon>
+            Liên hệ tạo trang thanh toán
+        </a>
+    </div>
+    <div class="modal deactive">
+        <div class="modal-top">
+            <img src="https://thongtinthanhtoan.com/theme/profile/images/momo.jpg" alt="Transfer now">
+            <h1 class="modal-title">CHUYỂN TIỀN NHANH CHÓNG</h1>
+            <h1 class="modal-content">
+                Chuyển tiền ngay thông qua app Momo bằng một lần chạm
+            </h1>
+        </div>
+        <div class="modal-bottom">
+            <span class="modal-btn" id="transfer">CHUYỂN NGAY</span>
+            <span class="modal-btn" id="dismiss">SAO CHÉP</span>
+        </div>
+    </div>
+    <script src="https://thongtinthanhtoan.com/theme/profile/js/index.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
